b3f75da019a10f8eaf6a9d237e3b151cd4fc8da6
parthdave11 <77158501+parthdave11@users.noreply.github.com>
2024-02-29 09:47:33 +0000

    IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,2 +1,19 @@
 // Immediately Invoked Function Expression (IIFE)
 
+// function chai(){
+//     console.log(`DB Connected`);
+// }
+
+// chai()
+
+
+//we use IIFE to prevent our function from global scope pollution.
+
+(function chai(){
+    // named IIFE
+    console.log(`DB Connected`);
+})(); // using IIFE you have to use semicolan.
+
+( (name) =>{
+    console.log(`DB Connected two ${name}`);
+} )('parth');
